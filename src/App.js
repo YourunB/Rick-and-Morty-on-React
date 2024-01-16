7b1492cd75b0da2719f32190ac2517e252a84dc4
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2024-01-17 00:27:51 +0300

    feat: add Footer, SpaceStars, PagesRouter to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import { PagesRouter } from './routes/PagesRouter';
+import Footer from './components/Footer';
+import SpaceStars from './components/SpaceStars';
+
 export const App = () => (
   <BrowserRouter>
+    <main className='app'>
+      <PagesRouter/>
+    </main>
+    <Footer/>
+    <SpaceStars/>
   </BrowserRouter>
 );
