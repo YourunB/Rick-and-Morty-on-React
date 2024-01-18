4362ee7ddbf6ba184c678b82d2740e158b8663ee
Yury Butskevich <bxyura@gmail.com>
2024-01-18 04:22:18 +0300

    feat: add btnToTop

--- src/pages/PageMain.js
+++ src/pages/PageMain.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import "regenerator-runtime/runtime";
 import { useDispatch, useSelector } from 'react-redux';
 import { updateSaveCharacters, upadteScrollCharacters, updatePage, updateLoad } from "../redux/dataSlice.js";
 import { NavLink } from 'react-router-dom';
-import {appEvents} from '../components/events.js';
 
+import { appEvents } from '../components/events.js';
 import { PagesLinks } from '../components/PagesLinks';
 import PageBackground from '../components/PageBackground';
 import Card from '../components/Card';
@@ -17,6 +17,8 @@ export const PageMain = () => {
   const [pagination, setPagination] = useState(false);
   const [checkedCard, setCheckedCard] = useState(null);
 
+  const btnToTop = useRef(null);
+
   const dispatch = useDispatch();
   const dataRedux = useSelector( state => state.data );
 
@@ -25,8 +27,8 @@ export const PageMain = () => {
     appEvents.addListener('EventCloseCardModal', closeCardModal);
     window.addEventListener("scroll", scrollLoad);
     return () => {
-      //appEvents.removeEventListener('EventCheckCard', checkCard);
-      //appEvents.removeEventListener('EventCloseCardModal', closeCardModal);
+      appEvents.removeListener('EventCheckCard', checkCard);
+      appEvents.removeListener('EventCloseCardModal', closeCardModal);
       window.removeEventListener("scroll", scrollLoad);
     }
   }, [dataRedux]);
@@ -44,6 +46,9 @@ export const PageMain = () => {
     const scrolled = window.scrollY;
     const threshold = height - screenHeight / 4;
     const position = scrolled + screenHeight;
+
+    if (scrolled > 400) btnToTop.current.className = 'PageMain__btn to-top';
+    else btnToTop.current.className = 'PageMain__btn to-top to-top_hide';
 
     if (position >= threshold && dataRedux.page < 42 && dataRedux.load === false && pagination === false) {
       dispatch(updatePage(dataRedux.page + 1));
@@ -124,6 +129,10 @@ export const PageMain = () => {
     locationNav();
   }, [window.location.search]);
 
+  function scrollToTop() {
+    window.scroll({top: 0, behavior: "smooth"});
+  }
+
   let cardCode;
   if (dataRedux.charactersArr !== null) {
     cardCode = dataRedux.charactersArr.map( el => {
@@ -147,9 +156,10 @@ export const PageMain = () => {
       <PagesLinks/>
       <section className='PageMain__head'>
         <h1 className='PageMain__head__title'>Rick and Morty Characters</h1>
-        <label className='PageMain__head__checkbox'>
-          Pagination: <input type={'checkbox'} checked={pagination} onChange={(e) => {changePagination(e)}}></input>
-        </label>
+        <div>
+          <input className='PageMain__head__input' id='select-pagination' type={'checkbox'} checked={pagination} onChange={(e) => {changePagination(e)}}></input>
+          <label className='PageMain__head__label' htmlFor='select-pagination'>Pagination:</label>
+        </div>
       </section>
       <section className='PageMain__cards'>
       {pagination === true ?
@@ -164,6 +174,7 @@ export const PageMain = () => {
         </div> : null}
         {cardCode}
       </section>
+      <button ref={btnToTop} className='PageMain__btn to-top' onClick={scrollToTop}>&#8657;</button>
     </main>
   );
     
